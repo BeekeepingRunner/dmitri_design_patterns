1ddcf0364a0f3369db9493b6b159ecd25e491c89
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-12-21 20:36:56 +0100

    journal writes and removes entries, persistence handles persistence

---
+++ src/solid/SingleResponsibilityPattern.java
@@ -0,0 +1,58 @@
+package solid;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleResponsibilityPattern {
+
+    public static void main(String[] args) throws Exception {
+        Journal j = new Journal();
+        j.addEntry("I cried today");
+        j.addEntry("I ate a bug");
+        System.out.println(j);
+
+        Persistence p = new Persistence();
+        String filename = "d:\\journal.txt";
+        p.saveToFile(j, filename, true);
+
+        Runtime.getRuntime().exec("notepad.exe " + filename);
+    }
+}
+
+class Journal {
+    private final List<String> entries = new ArrayList<>();
+    private static int count = 0;
+
+    public void addEntry(String text) {
+        entries.add("" + (++count) + ": " + text);
+    }
+
+    public void removeEntry(int index) {
+        entries.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(System.lineSeparator(), entries);
+    }
+}
+
+class Persistence {
+
+    public void saveToFile(Journal journal, String filename, boolean overwrite) throws FileNotFoundException {
+        if (overwrite || new File(filename).exists()) {
+            try (PrintStream out = new PrintStream(filename)) {
+                out.println(journal.toString());
+            }
+        }
+    }
+
+    /*
+    public Journal load(String filename) {}
+    public Journal load(URL url) {}
+
+     */
+}
