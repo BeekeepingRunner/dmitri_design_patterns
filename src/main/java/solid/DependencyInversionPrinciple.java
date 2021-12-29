1a1c45f053d41be0574f46463de14eb4298bf2c5
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-12-29 21:02:42 +0100

    make a high-level module dependent upon a low-level one

--- src/main/java/solid/DependencyInversionPrinciple.java
+++ src/main/java/solid/DependencyInversionPrinciple.java
@@ -7,7 +7,18 @@ import org.javatuples.Triplet;
 
 public class DependencyInversionPrinciple {
 
+    public static void main(String[] args) {
 
+        Person parent = new Person("John");
+        Person child1 = new Person("Chris");
+        Person child2 = new Person("Matt");
+
+        Relationships relationships = new Relationships();
+        relationships.addParentAndChild(parent, child1);
+        relationships.addParentAndChild(parent, child2);
+
+        new Research(relationships);
+    }
 }
 
 enum Relationship {
@@ -26,8 +37,32 @@ class Person {
     }
 }
 
+// low-level module
 class Relationships {
 
     private List<Triplet<Person, Relationship, Person>> relations
             = new ArrayList<>();
+
+    public List<Triplet<Person, Relationship, Person>> getRelations() {
+        return relations;
+    }
+
+    public void addParentAndChild(Person parent, Person child) {
+        relations.add(new Triplet<>(parent, Relationship.PARENT, child));
+        relations.add(new Triplet<>(child, Relationship.CHILD, parent));
+    }
+}
+
+// high-level module
+class Research {
+
+    public Research(Relationships relationships) {
+        List<Triplet<Person, Relationship, Person>> relations = relationships.getRelations();
+        relations.stream()
+                .filter(relation -> relation.getValue0().name.equals("John")
+                && relation.getValue1() == Relationship.PARENT)
+                .forEach(relation -> System.out.println(
+                        "John has a child called " + relation.getValue2().name
+                ));
+    }
 }
