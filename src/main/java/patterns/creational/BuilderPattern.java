6420c411092ecd23380beb4dd6d4784ede8bf24c
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-12-30 20:58:24 +0100

    create builder of nested html elements

--- src/main/java/patterns/creational/BuilderPattern.java
+++ src/main/java/patterns/creational/BuilderPattern.java
@@ -1,19 +1,85 @@
 package patterns.creational;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BuilderPattern {
 
     public static void main(String[] args) {
 
-        String hello = "hello";
-        System.out.println("<p>" + hello + "</p>");
+        HtmlBuilder builder = new HtmlBuilder("ul");
+        builder.addChild("li", "hello");
+        builder.addChild("li", "world");
+        System.out.println(builder);
+    }
+}
 
-        String[] words = {"hello", "world"};
+
+class HtmlBuilder {
+
+    private String rootName;
+    private HtmlElement root = new HtmlElement();
+
+    public HtmlBuilder(String rootName) {
+        this.rootName = rootName;
+        root.name = rootName;
+    }
+
+    public void addChild(String childName, String childText) {
+
+        HtmlElement el = new HtmlElement(childName, childText);
+        root.elements.add(el);
+    }
+
+    public void clear() {
+        root = new HtmlElement();
+        root.name = rootName;
+    }
+
+    @Override
+    public String toString() {
+        return root.toString();
+    }
+}
+
+class HtmlElement {
+
+    public String name;
+    public String text;
+    public ArrayList<HtmlElement> elements = new ArrayList<>();
+
+    private final int indentSize = 2;
+    private final String newLine = System.lineSeparator();
+
+    public HtmlElement() {
+    }
+
+    public HtmlElement(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    private String toStringImpl(int indentLevel) {
+
         StringBuilder sb = new StringBuilder();
-        sb.append("<ul>\n");
-        for (String word : words) {
-            sb.append(String.format("  <li>%s</li>\n", word));
+        String indent = String.join("", Collections.nCopies(indentLevel * indentSize, " "));
+        sb.append(String.format("%s<%s>%s", indent, name, newLine));
+
+        if (text != null && !text.isEmpty()) {
+            sb.append(String.join("", Collections.nCopies(indentSize * (indentLevel + 1), " ")))
+                    .append(text)
+                    .append(newLine);
         }
-        sb.append("</ul>");
-        System.out.println(sb);
+
+        for (HtmlElement e : elements)
+            sb.append(e.toStringImpl(indentLevel + 1));
+
+        sb.append(String.format("%s</%s>%s", indent, name, newLine));
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return toStringImpl(0);
     }
 }
