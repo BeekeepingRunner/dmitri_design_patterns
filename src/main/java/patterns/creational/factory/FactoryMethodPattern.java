34a159a1495e368332778f9243075a715a190746
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-01-02 18:05:09 +0100

    factory method to the rescue, clear, two ways of creation

--- src/main/java/patterns/creational/factory/FactoryMethodPattern.java
+++ src/main/java/patterns/creational/factory/FactoryMethodPattern.java
@@ -4,12 +4,8 @@ public class FactoryMethodPattern {
 
     public static void main(String[] args) {
 
+        Point point = Point.newPolarPoint(2, 3);
     }
-}
-
-enum CoordinateSystem {
-    CARTESIAN,
-    POLAR
 }
 
 class Point {
@@ -17,22 +13,18 @@ class Point {
     private double x;
     private double y;
 
-    /**
-     *
-     * @param a i x if cartesian or rho if polar
-     * @param b ...
-     * @param cs ...
-     */
-    public Point(double a, double b, CoordinateSystem cs) {
-        switch (cs) {
-            case CARTESIAN -> {
-                this.x = x;
-                this.y = y;
-            }
-            case POLAR -> {
-                x = a * Math.cos(b);
-                y = a * Math.sin(b);
-            }
-        }
+    private Point(double a, double b) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point newCartesianPoint(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public static Point newPolarPoint(double rho, double theta) {
+        return new Point(
+                rho * Math.cos(theta),
+                rho * Math.sin(theta));
     }
 }
