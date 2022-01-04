ffed6f3e7ad3e5a8380ee378caeeee051a8a0e53
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-01-04 19:14:49 +0100

    serialization can break the singleton

--- src/main/java/patterns/creational/singleton/Demo.java
+++ src/main/java/patterns/creational/singleton/Demo.java
@@ -1,16 +1,52 @@
 package patterns.creational.singleton;
 
+import java.io.*;
+
 public class Demo {
 
-    public static void main(String[] args) {
+    static void saveToFile(BasicSingleton singleton, String filename)
+        throws Exception {
+        try (
+                FileOutputStream fileOut = new FileOutputStream(filename);
+                ObjectOutputStream out = new ObjectOutputStream(fileOut))
+        {
+            out.writeObject(singleton);
+        }
+    }
+
+    static BasicSingleton readFromFile(String filename)
+        throws Exception {
+        try (
+                FileInputStream fileIn = new FileInputStream(filename);
+                ObjectInputStream in = new ObjectInputStream(fileIn)
+                )
+        {
+            return (BasicSingleton) in.readObject();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        // PROBLEMS which can break the singleton mechanism
+        // 1. reflection
+        // 2. serialization
 
         BasicSingleton singleton = BasicSingleton.getInstance();
-        singleton.setValue(123);
+        singleton.setValue(111);
+
+        final String filename = "singleton.bin";
+        saveToFile(singleton, filename);
+
+        singleton.setValue(222);
+
+        BasicSingleton singleton2 = readFromFile(filename);
+        System.out.println(singleton == singleton2);
         System.out.println(singleton.getValue());
+        System.out.println(singleton2.getValue());
     }
 }
 
-class BasicSingleton {
+class BasicSingleton implements Serializable {
 
     private BasicSingleton() {
 
