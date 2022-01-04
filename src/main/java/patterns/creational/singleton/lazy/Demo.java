3dedd94a9608141c32e8048f026cd721ea13117d
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-01-04 19:30:22 +0100

    thread-safety methods

--- src/main/java/patterns/creational/singleton/lazy/Demo.java
+++ src/main/java/patterns/creational/singleton/lazy/Demo.java
@@ -12,10 +12,26 @@ class LazySingleton {
         System.out.println("initializing a lazy singleton");
     }
 
-    // not thread safe
-    public static LazySingleton getInstance() {
+    /* FIX BY SYNCHRONIZED KEYWORD
+    public static synchronized LazySingleton getInstance() {
+
         if (instance == null) {
             instance = new LazySingleton();
+        }
+
+        return instance;
+    }
+     */
+
+    // double-checked locking
+    public static LazySingleton getInstance() {
+        
+        if (instance == null) {
+            synchronized (LazySingleton.class) {
+                if (instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
         }
 
         return instance;
