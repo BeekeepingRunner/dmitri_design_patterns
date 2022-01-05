47d407190f9a48933a98f2efe422948d821ce046
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-01-05 15:49:44 +0100

    create singleton database with utility class

--- src/main/java/patterns/creational/singleton/testing/SingletonRecordFinder.java
+++ src/main/java/patterns/creational/singleton/testing/SingletonRecordFinder.java
@@ -1,2 +1,14 @@
-package patterns.creational.singleton.testing;public class SingletonRecordFinder {
+package patterns.creational.singleton.testing;
+
+import java.util.List;
+
+public class SingletonRecordFinder {
+
+    public int getTotalPopulation(List<String> cityNames) {
+        int result = 0;
+        for (String name : cityNames) {
+            result += SingletonDatabase.getInstance().getPopulation(name);
+        }
+        return result;
+    }
 }
