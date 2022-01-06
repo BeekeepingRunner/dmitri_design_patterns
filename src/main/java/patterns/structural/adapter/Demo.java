abc78a895ab613de14f754449abb6837b5e757fc
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-01-06 13:40:20 +0100

    adapt lines from vectorobjects to points for drawing

--- src/main/java/patterns/structural/adapter/Demo.java
+++ src/main/java/patterns/structural/adapter/Demo.java
@@ -17,9 +17,21 @@ public class Demo {
         System.out.println(".");
     }
 
+    private static void draw() {
+
+        for (VectorObject vo : vectorObjects)
+        {
+            for (Line line : vo)
+            {
+                LineToPointAdapter adapter = new LineToPointAdapter(line);
+                adapter.forEach(Demo::drawPoint);
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
-
+        draw();
     }
 }
 
@@ -64,5 +76,39 @@ class VectorRectangle extends VectorObject {
         add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
         add(new Line(new Point(x, y), new Point(x, y + height)));
         add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+    }
+}
+
+class LineToPointAdapter extends ArrayList<Point> {
+
+    private static int count = 0;
+
+    public LineToPointAdapter(Line line) {
+        System.out.println(
+                String.format("%d: Generating points for line [%d, %d] - [%d, %d] (no caching)",
+                        ++count, line.start.x, line.start.y, line.end.x, line.end.y)
+        );
+
+        int left = Math.min(line.start.x, line.end.x);
+        int right = Math.max(line.start.x, line.end.x);
+        int top = Math.min(line.start.y, line.end.y);
+        int bottom = Math.max(line.start.y, line.end.y);
+        int dx = right - left;
+        int dy = line.end.y - line.start.y;
+
+        if (dx == 0)
+        {
+            for (int y = top; y <= bottom; ++y)
+            {
+                add(new Point(left, y));
+            }
+        }
+        else if (dy == 0)
+        {
+            for (int x = left; x <= right; ++x)
+            {
+                add(new Point(x, top));
+            }
+        }
     }
 }
