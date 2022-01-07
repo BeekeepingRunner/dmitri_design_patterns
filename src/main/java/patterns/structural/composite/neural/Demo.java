04dcf31d1503df5882fdb1858545dce07a6a19d1
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-01-07 17:43:09 +0100

    basic neural network design, but only one connection method is possible (neuron to neuron) :(

---
+++ src/main/java/patterns/structural/composite/neural/Demo.java
@@ -0,0 +1,35 @@
+package patterns.structural.composite.neural;
+
+import java.util.ArrayList;
+
+class Neuron {
+
+    public ArrayList<Neuron> in, out;
+
+    public void connectTo(Neuron other) {
+        out.add(other);
+        other.in.add(this);
+    }
+}
+
+class NeuronLayer extends ArrayList<Neuron> {
+
+
+}
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Neuron neuron = new Neuron();
+        Neuron neuron2 = new Neuron();
+
+        NeuronLayer layer = new NeuronLayer();
+        NeuronLayer layer2 = new NeuronLayer();
+        
+        neuron.connectTo(neuron2);
+        neuron.connectTo(layer);
+        layer.connectTo(neuron);
+        layer.connectTo(layer2);
+    }
+}
