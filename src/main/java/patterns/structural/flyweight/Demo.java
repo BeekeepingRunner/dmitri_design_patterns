9d467a1e8a411ddfcb583158896400879560966b
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-01-20 12:19:22 +0100

    store and use names that come up many times

--- src/main/java/patterns/structural/flyweight/Demo.java
+++ src/main/java/patterns/structural/flyweight/Demo.java
@@ -1,5 +1,10 @@
 package patterns.structural.flyweight;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 public class Demo {
 
     public static void main(String[] args) {
@@ -9,7 +14,28 @@ public class Demo {
     }
 }
 
+class User2 {
 
+    static List<String> strings = new ArrayList<>();
+    private int [] names;
+
+    public User2(String fullName) {
+
+        Function<String, Integer> getOrAdd = (String s) -> {
+            int idx = strings.indexOf(s);
+            if (idx != -1)
+                return idx;
+            else {
+                strings.add(s);
+                return strings.size() - 1;
+            }
+        };
+
+        names = Arrays.stream(fullName.split(" "))
+                .mapToInt(getOrAdd::apply)
+                .toArray();
+    }
+}
 
 class User {
 
