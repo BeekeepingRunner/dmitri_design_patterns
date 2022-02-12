01a0916b6bd62e3073ade09359d041464a8ce977
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-02-12 17:19:21 +0100

    store properties as array using iterator

--- src/main/java/patterns/behavioral/iterator/arraybackedproperties/Demo.java
+++ src/main/java/patterns/behavioral/iterator/arraybackedproperties/Demo.java
@@ -1,5 +1,10 @@
 package patterns.behavioral.iterator.arraybackedproperties;
 
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
 class SimpleCreature {
 
     private int strength;
@@ -43,5 +48,71 @@ class SimpleCreature {
     }
 }
 
+class Creature implements Iterable<Integer>
+{
+    private int [] stats = new int[3];
+
+    private final int strength = 0;
+    private final int agility = 1;
+    private final int intelligence = 2;
+
+    public int getStrength() { return stats[strength]; }
+
+    public void setStrength(int value) {
+        stats[strength] = value;
+    }
+
+    public int getAgility() { return stats[agility]; }
+
+    public void setAgility(int value) {
+        stats[agility] = value;
+    }
+
+    public int getIntelligence() { return stats[intelligence]; }
+
+    public void setIntelligence(int value) {
+        stats[intelligence] = value;
+    }
+
+    public int sum() {
+        return IntStream.of(stats).sum();
+    }
+
+    public int max() {
+        return IntStream.of(stats).max().getAsInt();
+    }
+
+    public double average() {
+        return IntStream.of(stats).average().getAsDouble();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return IntStream.of(stats).iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+        for (int x : stats)
+            action.accept(x);
+    }
+
+    @Override
+    public Spliterator<Integer> spliterator() {
+        return IntStream.of(stats).spliterator();
+    }
+}
+
 public class Demo {
+
+    public static void main(String[] args) {
+
+        final Creature creature = new Creature();
+        creature.setAgility(12);
+        creature.setIntelligence(13);
+        creature.setStrength(17);
+        System.out.println("Creature has a max stat of " + creature.max()
+        + ", total stats = " + creature.sum()
+        + ", average stat = " + creature.average());
+    }
 }
